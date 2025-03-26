bf8985e3a9d644c6a313df1646a56a4eef9c5fb6
Mmohima Modak <mohimamodak316@gmail.com>
2025-03-26 16:21:01 +0600

    Your commit message

--- src/assets/Component/Navbar/Navbar.jsx
+++ src/assets/Component/Navbar/Navbar.jsx
@@ -15,15 +15,15 @@ export default function Navbar() {
 
   return (
     <div className="fixed w-full text-white font-medium top-0 z-50">
-      
+
       <div className="md:hidden sm:block text-sm" onClick={toggleButton}>
         <span className="cursor-pointer p-5"><img src={menu} className="w-10" alt="menu" /></span>
       </div>
       <div className={`mobile-menu ${toggle ? "open" : ""}`}>
-      <div className=" flex justify-center items-center gap-2">
-            <img src={logo} className="w-12" />
-              <h1 className="text-2xl lg:text-3xl font-bold text-blue-600">HealthCare</h1>
-            </div>
+        <div className=" flex justify-center items-center gap-2">
+          <img src={logo} className="w-12" />
+          <h1 className="text-2xl lg:text-3xl font-bold text-blue-600">HealthCare</h1>
+        </div>
         <div className="navbar-links text-blue-600 font-bold">
           <Link to="/" smooth={true} duration={1000} className="text-xl p-4 block" onClick={toggleButton}>
             Home
@@ -43,38 +43,48 @@ export default function Navbar() {
           <Link to="/Contact" smooth={true} duration={1000} className="text-xl p-4 block" onClick={toggleButton}>
             Contact
           </Link>
+          <div>
+            <a target='_blank' href="https://github.com/MohimaModak/HealthCare">
+              <button className='text-lg font-bold inline-block text-center'>Github & Doc</button>
+            </a>
+          </div>
         </div>
       </div>
 
-    
+
       <div className="flex justify-center items-center">
         <div className="hidden md:block bg-sky-50 text-blue-500 w-full">
           <nav className="navbar flex justify-between items-center gap-2">
 
             <div className=" flex justify-center items-center gap-2">
-            <img src={logo} className="w-14" />
+              <img src={logo} className="w-14" />
               <h1 className="text-2xl lg:text-3xl font-bold">HealthCare</h1>
             </div>
 
             <div className="navbar-links lg:flex justify-evenly items-center">
-              <Link to="/" smooth={true} duration={1000} className="text-xl mr-5 font-semibold">
+              <Link to="/" smooth={true} duration={1000} className="text-lg mr-5 font-semibold">
                 Home
               </Link>
-              <Link to="/About" smooth={true} duration={1000} className="text-xl mr-5 font-semibold">
+              <Link to="/About" smooth={true} duration={1000} className="text-lg mr-5 font-semibold">
                 About
               </Link>
-              <Link to="/Services" smooth={true} duration={1000} className="text-xl mr-5 font-semibold">
+              <Link to="/Services" smooth={true} duration={1000} className="text-lg mr-5 font-semibold">
                 Services
               </Link>
-              <Link to="/Insurance" smooth={true} duration={1000} className="text-xl mr-5 font-semibold">
+              <Link to="/Insurance" smooth={true} duration={1000} className="text-lg mr-5 font-semibold">
                 Insurance
               </Link>
-              <Link to="/Testimonials" smooth={true} duration={1000} className="text-xl mr-5 font-semibold">
+              <Link to="/Testimonials" smooth={true} duration={1000} className="text-lg mr-5 font-semibold">
                 Testimonial
               </Link>
-              <Link to="/Contact" smooth={true} duration={1000} className="text-xl font-semibold">
+              <Link to="/Contact" smooth={true} duration={1000} className="text-lg mr-5 font-semibold">
                 Contact
               </Link>
+              <div>
+                <a target='_blank' href="https://github.com/MohimaModak/HealthCare">
+                  <button className='text-lg font-bold inline-block text-center'>Github & Doc</button>
+                </a>
+              </div>
 
             </div>
 
